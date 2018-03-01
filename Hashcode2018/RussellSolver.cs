6d7a9f8390009b63b53a56c337b9f6a12038b5d2
Russell Goodison <russell.goodison@gmail.com>
2018-03-01 21:10:56 +0200

    Stuff

--- Hashcode2018/RussellSolver.cs
+++ Hashcode2018/RussellSolver.cs
@@ -18,4 +18,46 @@ namespace Hashcode2018
 
         }
     }
+
+    public class Ride
+    {
+        public int id { get; set; }
+        public int startRow { get; set; }
+        public int startColumn { get; set; }
+
+        public int finx { get; set; }
+        public int finy { get; set; }
+
+        public int startTick { get; set; }
+        public int endTick { get; set; }
+
+        public Ride()
+        {
+
+        }
+    }
+
+    public class Vehicle
+    {
+
+        public List<Ride> scheduledRides { get; set; }
+        public int posX { get; set; }
+        public int posY { get; set; }
+        public Vehicle()
+        {
+            scheduledRides = new List<Ride>();
+            posX = 0;
+            posY = 0;
+        }
+        public override string ToString()
+        {
+            var s = scheduledRides.Count().ToString() + " ";
+            foreach (var item in scheduledRides)
+            {
+                s += item.id + " ";
+            }
+            return s;
+        }
+
+    }
 }
