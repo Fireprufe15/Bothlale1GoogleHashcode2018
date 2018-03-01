306d55fa0912674eba8d2aa91c6352da8367f871
Russell Goodison <russell.goodison@gmail.com>
2018-03-01 20:11:57 +0200

    Output Format Verification

---
+++ Hashcode2018/Verification.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hashcode2018
+{
+    public class Verification
+    {
+        public Verification()
+        {
+
+        }
+
+        public bool isValidFormat(List<string> result)
+        {
+            try
+            {
+                foreach (string item in result)
+                {
+                    string[] arrSplitter = item.Split(' ');
+                    for (int i = 0; i < arrSplitter.Length; i++)
+                    {
+                        int.Parse(arrSplitter[i]);
+                    }
+                }
+
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
