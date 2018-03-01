09cff8ceeb137539d1bdfad3448171b9554be2b8
Russell Goodison <russell.goodison@gmail.com>
2018-03-01 20:19:45 +0200

    Added In files

---
+++ Hashcode2018/RussellSolver.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hashcode2018
+{
+    public class RussellSolver
+    {
+        public RussellSolver()
+        {
+
+        }
+
+        public void Solve()
+        {
+
+        }
+    }
+}
